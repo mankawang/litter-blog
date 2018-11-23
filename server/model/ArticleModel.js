367cf0e7449e97b22b5264e9c626502b84c98208
mankawang <784247242@qq.com>
2018-11-23 21:03:40 +0800

    最后设计发表文章和删除文章

--- server/model/ArticleModel.js
+++ server/model/ArticleModel.js
@@ -1,4 +1,5 @@
 import query from '../utils/query'
+import eacape from '../utils/eacape'
 
 class ArticleModel {
     async getAllArticles(){
@@ -11,7 +12,7 @@ class ArticleModel {
         return await query(`SELECT * FROM ARTICLE WHERE id=${id}`)
     }
     async updateArticle(id,{title,tags,content,isPublished}){
-        return await query(`UPDATE ARTICLE SET title='${title}', tags='${tags}',content='${content}' WHERE id='${id}'`)
+        return await query(eacape`UPDATE ARTICLE SET title=${title}, tags=${tags},content=${content} WHERE id=${id}`)
     }
 }
 export default new ArticleModel() 
