2eb512fb88c910d13ada72d822b61f1a5d6b7db5
mankawang <784247242@qq.com>
2018-11-23 20:30:34 +0800

    添加新文章

--- admin/src/components/common/ArticleList.vue
+++ admin/src/components/common/ArticleList.vue
@@ -1,13 +1,97 @@
 <template>
-    
+  <div>
+    <ul class="list">
+      <li class="article"  :class="{active: activeIndex === index, published: isPublished === 1}" v-for="{title,createTime,isPublished,isChosen},index in articleList">
+        <header>{{title}}</header>
+        <p>{{createTime}}</p>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: "ArticleList"
+  import request from '@/utils/request'
+  import moment from 'moment'
+  import { mapState,mapMutations } from 'vuex'
+  export default {
+    name: "ArticleList",
+    data(){
+      return {
+        articleList:[],
+        activeIndex:-1
+      }
+    },
+    //把全局的vuex里面的state和mutations放到计算属性中...
+    computed:{
+      ...mapState(['id','title','tags','content','isPublished']),
+      ...mapMutations(['SET_CURRENT_ARTICLE'])
+      //...mapActions([''])
+    },
+    //当该组件创建的时候自动执行里面的请求
+    created(){
+      request({
+        method:'get',
+        url:'/articles'
+      }).then(res=>{
+        for(let article of res){
+          article.createTime = moment(article.createTime).format('YYYY年-MM月-DD日 HH:mm:ss')
+          article.isChosen = true
+        }
+        this.articleList.push(...res)
+      }).catch(err=>{
+        console.log(err)
+      })
+    },
+    methods:{
+      updateList(updateId){
+        request({
+          method:"get",
+          url:`/articles/${updateId}`
+        }).then(res=>{
+          const article = res[0]
+          article.createTime = moment(article.createTime).format('YYYY年-MM月-DD日 HH:mm:ss')
+          article.isChosen = true
+          this.articleList.unshift(article)
+        }).catch(err=>{
+          console.log(err);
+        })
+      }
     }
+  }
 </script>
 
-<style scoped>
-
+<style type="text/scss"  lang="scss" scoped>
+  /*记得引入全局变量的文件*/
+  @import '../../assets/style/variable';
+  .article {
+    @include flex($flow: column wrap, $align: flex-start);
+    padding: 0.2em 0.5em;
+    margin-bottom: 1.5em;
+    height: 5em;
+    max-width: 100%;
+    background: $white;
+    border: 0.1em solid $special;
+    cursor: pointer;
+    header {
+      max-width: 100%;
+      font-size: 1.3rem;
+      white-space: nowrap;
+      overflow: hidden;
+      text-overflow: ellipsis;
+      margin-bottom: 0.2em;
+    }
+    p {
+      margin: 0;
+      color: $special;
+    }
+    &:last-child {
+      margin-bottom: 0;
+    }
+  }
+  .active {
+    border: 1px solid $base;
+  }
+  .published {
+    border-right: 4px solid $base;
+  }
 </style>
--- server/routes/index.js
+++ server/routes/index.js
@@ -2,6 +2,10 @@ import Router from 'koa-router'
 import { base_API } from "../config"
 //引入我们的登录注册控制器
 import UserController from '../Controller/UserController'
+//引入文章相关的控制器
+import ArticleController from '../Controller/ArticleController'
+//引入验证token合法性的方法
+import checkToken from '../utils/checkToken'
 
 const router = new Router()
 router.get('/',async ctx=>{
@@ -13,5 +17,13 @@ router.prefix(`${base_API}`)
 
 //登录
 router.post('/login',UserController.login)
-
-export default router
+//退出
+//只要是登录后才能看到的内容都需要验证一下token的合法性.
+router.get('/logout',checkToken,UserController.logout)
+//获取所有的文章
+router.get('/articles',checkToken,ArticleController.getArticles)
+//添加一篇新的文章
+router.post('/articles/add',checkToken,ArticleController.addArticle)
+//查询一下最新发布的那片文章的详细信息
+router.get('/articles/:id',checkToken,ArticleController.getOneArticle)
+export default router 
